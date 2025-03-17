67c78e2f0bf2a569c0ea73e7241eb9a7915ac2ef
MK <99416359+MK316@users.noreply.github.com>
2025-03-18 01:08:31 +0900

    Update 🌿Lesson_03.py

--- pages/🌿Lesson_03.py
+++ pages/🌿Lesson_03.py
@@ -14,8 +14,8 @@ with tab1:
     
     # Define the correct answers (update these as per your quiz answers)
     correct_answers = {
-        1: 2, 2: 3, 3: 1, 4: 2, 5: 1,
-        6: 3, 7: 2, 8: 1, 9: 3, 10: 2
+        1: 3, 2: 2, 3: 2, 4: 1, 5: 3,
+        6: 3, 7: 1, 8: 3, 9: 1, 10: 1
     }
 
     # Provide a single audio file that contains all questions
